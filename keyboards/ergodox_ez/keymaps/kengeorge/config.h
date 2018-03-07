005e4adcc615c943f651ddc340852b7faeb55f25
Ken George <ken@whatever.email>
2018-03-07 14:47:45 -0800

    Added config.h for mouse settings.

---
+++ keyboards/ergodox_ez/keymaps/kengeorge/config.h
@@ -0,0 +1,11 @@
+#include "../../config.h"
+
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_MAX_SPEED
+
+#define MOUSEKEY_INTERVAL       11
+#define MOUSEKEY_DELAY          1
+#define MOUSEKEY_TIME_TO_MAX    50
+#define MOUSEKEY_MAX_SPEED      2
