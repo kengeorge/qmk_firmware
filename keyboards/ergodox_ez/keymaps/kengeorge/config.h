601e2362aed1c17e1c478aa6a9511e59139fff96
Ken George <kggeorge@amazon.com>
2019-04-11 17:16:06 -0700

    Increasing debounce to stop repeated key presses

--- keyboards/ergodox_ez/keymaps/kengeorge/config.h
+++ keyboards/ergodox_ez/keymaps/kengeorge/config.h
@@ -1,11 +1,16 @@
 #include "../../config.h"
 
 #undef MOUSEKEY_INTERVAL
-#undef MOUSEKEY_DELAY
-#undef MOUSEKEY_TIME_TO_MAX
-#undef MOUSEKEY_MAX_SPEED
-
 #define MOUSEKEY_INTERVAL       11
+
+#undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY          1
+
+#undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX    50
+
+#undef MOUSEKEY_MAX_SPEED
 #define MOUSEKEY_MAX_SPEED      2
+
+#undef DEBOUNCE
+#define DEBOUNCE 18
